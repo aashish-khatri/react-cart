938f1d1278eae8e2ce1003c4a451a3d83ac7262f
Aashish Khatri <aashishkhari809@gmail.com>
2020-06-17 14:37:29 +0530

    Updates

---
+++ precache-manifest.416c529425d287ae70c7b3efe97386f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d83f4cac4f3b7ae2bdb0ecb2d3c827a",
+    "url": "/react-cart/index.html"
+  },
+  {
+    "revision": "6e2000b360d68ce34316",
+    "url": "/react-cart/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "3851e6a56cc5237b9cdd",
+    "url": "/react-cart/static/js/2.8b64f330.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cart/static/js/2.8b64f330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e2000b360d68ce34316",
+    "url": "/react-cart/static/js/main.4fbcb6b2.chunk.js"
+  },
+  {
+    "revision": "0dcf588f8d3918a369a8",
+    "url": "/react-cart/static/js/runtime-main.48745bcc.js"
+  }
+]);
